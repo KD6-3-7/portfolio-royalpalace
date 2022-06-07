36529fcc5b3d15b806f1e3b331f097f4f1a32523
DongKeon94 <compuer1@naver.com>
2022-06-07 23:01:45 +0900

    6/07 converse 수정

--- sub/history/js/converse.js
+++ sub/history/js/converse.js
@@ -1,38 +1,3 @@
-// window.onload = function() {
-
-//     const clickOne = document.getElementById("clickOne");
-//     const clickTwo = document.getElementById("clickTwo");
-//     const clickThr = document.getElementById("clickThr");
-//     const clickFour = document.getElementById("clickFour");
-//     const clickFive = document.getElementById("clickFive");
-    
-//     const showOne = document.querySelector(".show-one");
-//     const showTwo = document.querySelector(".show-two");
-//     const showThr = document.querySelector(".show-thr");
-//     const showFour = document.querySelector(".show-four");
-//     const showFive = document.querySelector(".show-five");
-
-//     clickOne.addEventListener("click", function(){
-//         showOne.style.display = "block";
-//     });
-
-//     clickTwo.addEventListener("click", function(){
-//         showTwo.style.display = "block";
-//     });
-
-//     clickThr.addEventListener("click", function(){
-//         showThr.style.display = "block";
-//     });
-
-//     clickFour.addEventListener("click", function(){
-//         showFour.style.display = "block";
-//     });
-
-//     clickFive.addEventListener("click", function(){
-//         showFive.style.display = "block";
-//     });
-// }
-
 $(function() {
     const del = $(".show-one, .show-two, .show-thr, .show-four, .show-five")
 
