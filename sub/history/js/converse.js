16af49cd3758b44e18a2a3f56db89253883d0637
DongKeon94 <compuer1@naver.com>
2022-06-08 12:01:16 +0900

    06/08 converse 수정

--- sub/history/js/converse.js
+++ sub/history/js/converse.js
@@ -1,29 +1,29 @@
 $(function() {
-    const del = $(".show-one, .show-two, .show-thr, .show-four, .show-five")
-    $(del).hide();
+    const hideImage = $(".show-one, .show-two, .show-thr, .show-four, .show-five")
+    $(hideImage).hide();
 
     $("#slideOne").on("click", function() {
-        $(del).hide();
+        $(hideImage).hide();
         $(".show-one").fadeIn(500);
     });
 
     $("#slideTwo").on("click", function() {
-        $(del).hide();
+        $(hideImage).hide();
         $(".show-two").fadeIn(500);
     });
 
     $("#slideThr").on("click", function() {
-        $(del).hide();
+        $(hideImage).hide();
         $(".show-thr").fadeIn(500);
     });
 
     $("#slideFour").on("click", function() {
-        $(del).hide();
+        $(hideImage).hide();
         $(".show-four").fadeIn(500);
     });
 
     $("#slideFive").on("click", function() {
-        $(del).hide();
+        $(hideImage).hide();
         $(".show-five").fadeIn(500);
     });
 });
